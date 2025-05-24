f81b486b9253bf33cdc0bbed43ccfe04c9dee0d2
danielxiquin <danielxiquin802@gmail.com>
2025-05-23 18:24:05 -0600

    se agrego link a seccion suscribe

--- src/components/Suscribe.jsx
+++ src/components/Suscribe.jsx
@@ -3,7 +3,12 @@ export default function Subscribe() {
     <section className="flex flex-col items-center justify-center min-h-[15rem] sm:min-h-[20rem] md:min-h-[25rem] lg:min-h-[30rem] p-4 gap-2 sm:gap-3 md:gap-4 text-center">
       <h1 className="text-main-text text-4xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-9xl font-semibold">INSCRIBETE</h1>
       <p className="text-secondary-text text-base sm:text-lg max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
-      <button className="bg-secondary w-32 sm:w-36 md:w-40 h-8 sm:h-9 md:h-10 hover:bg-[#fce6c7] mt-2 sm:mt-3 md:mt-4">Ir a inscripción</button>
+      <a
+        href="/signup"
+        className="bg-secondary w-32 sm:w-36 md:w-40 h-8 sm:h-9 md:h-10 hover:bg-[#fce6c7] mt-2 sm:mt-3 md:mt-4 flex items-center justify-center text-center"
+      >
+        Ir a inscripción
+      </a>
     </section>
   );
 }
