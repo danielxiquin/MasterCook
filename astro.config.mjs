2a6973fd3433ae66551b939e3f4ba38abaf493a6
danielxiquin <danielxiquin802@gmail.com>
2025-08-21 18:50:39 -0600

    se arreglo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
-    adapter: vercel({}),
     integrations: [react(), tailwind()],
     vite: {
     resolve: {
